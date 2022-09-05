0b241e47586e8749d4a79d2e6c3a2f0f738ceed5
Sofia <varismendi@udesa.edu.ar>
2022-09-05 12:34:13 -0300

    instalación de la api

---
+++ proyecto-integrador/src/components/Api/Api.js
@@ -0,0 +1,19 @@
+/*import React from 'react';
+
+const componentDidMount = () => {
+    const url = "https://api.themoviedb.org/3/movie/550?api_key=d3bf40c9b6ae8b0603c799bd0fc81e36"
+
+    fetch (url)
+    .then (res => res.json())
+    .then (data => {
+        console.log(data.id);
+        console.log(data.adult);
+    })
+    .catch (err => {
+        console.log(err.message);
+    });
+    return (
+        <div>componentDidMount</div>
+    ) 
+}
+export default componentDidMount */
